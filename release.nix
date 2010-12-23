dd31a9d9e57cbe67296c5c450e6493d4ee9fef11
Sander van der Burg <svanderburg@gmail.com>
2010-12-23 18:08:58 +0000

    Increased wait interval a bit, which hopefully fixes failures

--- release.nix
+++ release.nix
@@ -45,7 +45,7 @@ let
 	  ''
 	    # Start Firefox and take a screenshot
 	    
-	    $test3->mustSucceed("sleep 30; firefox http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk &");
+	    $test3->mustSucceed("sleep 60; firefox http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk &");
 	    $test3->waitForWindow(qr/Namoroka/);
 	    $test3->mustSucceed("sleep 30");  
 	    $test3->screenshot("screen");
