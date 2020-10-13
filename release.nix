3655a6c0ad97a7cc7f35a28fc840420049ca7d2e
Sander van der Burg <svanderburg@gmail.com>
2020-10-13 20:08:33 +0200

    Add testcase that combines service and container deployment

--- release.nix
+++ release.nix
@@ -1,5 +1,6 @@
 { nixpkgs ? <nixpkgs>
-, viewvc ? {outPath = ./.; rev = 1234;}
+, viewvc ? { outPath = ./.; rev = 1234; }
+, nix-processmgmt ? { outPath = ../nix-processmgmt; rev = 1234; }
 , officialRelease ? false
 , systems ? [ "i686-linux" "x86_64-linux" ]
 }:
@@ -20,37 +21,70 @@ let
       inherit officialRelease version;
     };
 
-    build = pkgs.lib.genAttrs systems (system:
-      let
-        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs system;
-        };
-      in
-      disnixos.buildManifest {
-        name = "viewvc";
-        inherit tarball version;
-        servicesFile = "DistributedDeployment/services.nix";
-        networkFile = "DistributedDeployment/network.nix";
-        distributionFile = "DistributedDeployment/distribution.nix";
-      });
+    build = {
+      services = pkgs.lib.genAttrs systems (system:
+        let
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs system;
+          };
+        in
+        disnixos.buildManifest {
+          name = "viewvc";
+          inherit tarball version;
+          servicesFile = "DistributedDeployment/services.nix";
+          networkFile = "DistributedDeployment/network.nix";
+          distributionFile = "DistributedDeployment/distribution.nix";
+        });
 
-    tests = disnixos.disnixTest {
-      name = "viewvc";
-      inherit tarball;
-      manifest = builtins.getAttr (builtins.currentSystem) build;
-      networkFile = "DistributedDeployment/network.nix";
-      testScript =
-        ''
-          test3.succeed("sleep 30; curl --fail http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk")
-
-          # Start Firefox and take a screenshot
-
-          test3.succeed("firefox http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk &")
-          test3.wait_for_window("Firefox")
-          test3.succeed("sleep 30")
-          test3.screenshot("screen")
-        '';
+      services-with-containers = pkgs.lib.genAttrs systems (system:
+        let
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs system;
+          };
+        in
+        disnixos.buildManifest {
+          name = "viewvc";
+          inherit tarball version;
+          servicesFile = "DistributedDeployment/services-with-containers.nix";
+          networkFile = "DistributedDeployment/network-bare.nix";
+          distributionFile = "DistributedDeployment/distribution-with-containers.nix";
+          extraParams = {
+            inherit nix-processmgmt;
+          };
+        });
     };
+
+    tests =
+      let
+        testApp = {networkFile, manifest}:
+
+        disnixos.disnixTest {
+          name = "viewvc";
+          inherit tarball manifest networkFile;
+          testScript =
+            ''
+              test3.succeed("sleep 30; curl --fail http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk")
+
+              # Start Firefox and take a screenshot
+
+              test3.succeed("firefox http://test1/viewvc/bin/cgi/viewvc.cgi/aefs/trunk &")
+              test3.wait_for_window("Firefox")
+              test3.succeed("sleep 30")
+              test3.screenshot("screen")
+            '';
+         };
+      in
+      {
+        services = testApp {
+          manifest = builtins.getAttr (builtins.currentSystem) build.services;
+          networkFile = "DistributedDeployment/network.nix";
+        };
+
+        services-with-containers = testApp {
+          manifest = builtins.getAttr (builtins.currentSystem) build.services-with-containers;
+          networkFile = "DistributedDeployment/network-bare.nix";
+        };
+      };
   };
 in
 jobs
